a94b7d030ac702cd3ad611edc2136151b2ecb05e
csynoers <3s0c9m7@gmail.com>
2019-10-29 21:11:50 +0700

    change config connection

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,9 +2,9 @@
 date_default_timezone_set("Asia/Jakarta");
 error_reporting(0);
 $server = "localhost";
-$username = "root";
-$password = "";
-$database = "db_noni";
+$username = "nsok7753_5c3";
+$password = "3.s.0.c.9.m.7";
+$database = "nsok7753_noni";
 
 // Koneksi dan memilih database di server
 mysql_connect($server,$username,$password) or die("Koneksi gagal");
