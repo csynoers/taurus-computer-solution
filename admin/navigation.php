e3ac0cf8582c4ce0d12a94a3b1ece895a699ae81
csynoers <3s0c9m7@gmail.com>
2019-11-01 02:58:47 +0700

    add menu support

--- admin/navigation.php
+++ admin/navigation.php
@@ -38,8 +38,15 @@ echo"<li>
 		<li>
           <a href='media.php?module=laporan'>
             <i class='fa fa-book'></i> <span>Laporan</span>
-            
           </a>
+		</li>
+		<li class='treeview'>
+          <a href='#'>
+            <i class='fa fa-laptop'></i> <span>Support</span> <i class='fa fa-angle-left pull-right'></i>
+          </a>
+          <ul class='treeview-menu'>
+		    <li><a href='media.php?module=servis'><i class='fa fa-circle-o'></i> Logo</a></li>
+          </ul>
         </li>
         ";
 }
