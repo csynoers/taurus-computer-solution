d4ab303be65eb1dc40718025fed1b541a67b954c
csynoers <3s0c9m7@gmail.com>
2019-11-01 03:41:07 +0700

    modul menu support:logo

---
+++ admin/modul/mod_logo/aksi_logo.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+if (empty($_SESSION['username']) AND empty($_SESSION['passuser'])){
+    echo "<link href='style.css' rel='stylesheet' type='text/css'>
+    <center>Untuk mengakses modul, Anda harus login <br>";
+    echo "<a href=../../index.php><b>LOGIN</b></a></center>";
+}
+
+else{
+
+    $module=$_GET['module'];
+    $act=$_GET['act'];
+    include "../../../config/helper_file.php";
+    include "../../../config/helper_upload.php";
+    
+    if ($module=='admin' AND $act=='update')
+    {
+        $json = '../../../json/json.json';
+        $row= read_file($json);
+
+        $filename = img_resize($_FILES['fupload'],193,'../../../src/logo/'); 
+        
+        if ( $filename != 'error' ) {
+            if( ($row->filename != '') && file_exists("../../../src/logo/{$row->filename}") ){
+                unlink("../../../src/logo/{$row->filename}");
+            }
+            overwrite_file($json,['filename'=>$filename]);
+        }
+
+        echo "<script>window.alert('Logo berhasil diubah');
+        window.location=('../../media.php?module={$module}')</script>";
+    }
+}
