09dcfd30a4df4efc1c106c940715f164d5d49b72
csynoers <3s0c9m7@gmail.com>
2019-11-01 02:33:48 +0700

    fix cek login admin

--- admin/cek_login.php
+++ admin/cek_login.php
@@ -1,22 +1,26 @@
 <?php
-include "../config/koneksi.php";
+	include "../config/koneksi.php";
 
-$username = $_POST['username'];
-$pass     = md5($_POST['password']);
+	$username 	= $_POST['username'];
+	$pass     	= md5($_POST['password']);
 
-$login=mysql_query("SELECT * FROM user WHERE username='$username' AND password='$pass' ");
-$ketemu=mysql_num_rows($login);
-$r=mysql_fetch_array($login);
+	$login		=mysql_query("SELECT * FROM user WHERE username='$username' AND password='$pass' ");
+	$ketemu		=mysql_num_rows($login);
+	$r			=mysql_fetch_array($login);
+
+	print_r($r);
+	die();
+
 
 // Apabila username dan password ditemukan
 if ($ketemu > 0){
-  session_start();
-  $_SESSION[namauser]     = $r[username];
-  $_SESSION[id_user]     = $r[id_user];
-  $_SESSION[namalengkap]  = $r[nama_lengkap];
-  $_SESSION[passuser]     = $r[password];
-  $_SESSION[leveluser]    = $r[level];
-  header('location:media.php?module=home');
+	session_start();
+	$_SESSION[namauser]     = $r[username];
+	$_SESSION[id_user]     = $r[id_user];
+	$_SESSION[namalengkap]  = $r[nama_lengkap];
+	$_SESSION[passuser]     = $r[password];
+	$_SESSION[leveluser]    = $r[level];
+	header('location:media.php?module=home');
 }
 else{
 
