64a13476d9045bc4a6d7b2a237ecfddd98e5f187
csynoers <3s0c9m7@gmail.com>
2019-11-01 01:52:56 +0700

    fix new media

--- media.php
+++ media.php
@@ -1,5 +1,23 @@
 <?php
     //start session
     session_start();
+
+    /* load config required */
+    include_once "config/koneksi.php";
+	include_once "config/fungsi_thumb.php";
+	include_once "config/fungsi_indotgl.php";
+	include_once "config/class_paging.php";
+	include_once "config/fungsi_combobox.php";
+	include_once "config/library.php";
+	include_once "config/fungsi_rupiah.php";
+	$succesUrl = $serverUrlAndPath."success.php";
+	$failUrl = $serverUrlAndPath."fail.php";
+    $statusUrl = $serverUrlAndPath."status.php";
+
+    echo '<pre>';
+	print_r($_SESSION);
+	echo '</pre>';
+    
+    /* load template */
     include_once('template.php');
     ob_end_flush();
--- template.php
+++ template.php
@@ -1,73 +1,49 @@
-﻿<?php
-	echo '<pre>';
-	print_r($_SESSION);
-	echo '</pre>';
-	// ob_start();
-	// session_start();	
-	// error_reporting(0);
-	include "config/koneksi.php";
-	include "config/fungsi_thumb.php";
-	include "config/fungsi_indotgl.php";
-	include "config/class_paging.php";
-	include "config/fungsi_combobox.php";
-	include "config/library.php";
-	include "config/fungsi_rupiah.php";
-	$succesUrl = $serverUrlAndPath."success.php";
-	$failUrl = $serverUrlAndPath."fail.php";
-	$statusUrl = $serverUrlAndPath."status.php";
-?>
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Taurus Computer Solution</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-<!--Less styles -->
-   <!-- Other Less css file //different less files has different color scheam
-	<link rel="stylesheet/less" type="text/css" href="themes/less/simplex.less">
-	<link rel="stylesheet/less" type="text/css" href="themes/less/classified.less">
-	<link rel="stylesheet/less" type="text/css" href="themes/less/amelia.less">  MOVE DOWN TO activate
-	-->
-	<!--<link rel="stylesheet/less" type="text/css" href="themes/less/bootshop.less">
-	<script src="themes/js/less.js" type="text/javascript"></script> -->
-	
-<!-- Bootstrap style --> 
-    <link id="callCss" rel="stylesheet" href="themes/bootshop/bootstrap.min.css" media="screen"/>
-    <link href="themes/css/base.css" rel="stylesheet" media="screen"/>
-<!-- Bootstrap style responsive -->	
-	<link href="themes/css/bootstrap-responsive.min.css" rel="stylesheet"/>
-	<link href="themes/css/font-awesome.css" rel="stylesheet" type="text/css">
-<!-- Google-code-prettify -->	
-	<link href="themes/js/google-code-prettify/prettify.css" rel="stylesheet"/>
-<!-- fav and touch icons -->
-    <link rel="shortcut icon" href="themes/images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="themes/images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="themes/images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="themes/images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="themes/images/ico/apple-touch-icon-57-precomposed.png">
-	<style type="text/css" id="enject"></style>
-  </head>
+	<head>
+		<meta charset="utf-8">
+		<title>Taurus Computer Solution</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Taurus Computer Solution">
+		<meta name="keywords" content="Taurus Computer Solution">
+		<meta name="author" content="csynoers">
+		
+		<!-- Bootstrap style --> 
+		<link id="callCss" rel="stylesheet" href="themes/bootshop/bootstrap.min.css" media="screen"/>
+		<link href="themes/css/base.css" rel="stylesheet" media="screen"/>
+		<!-- Bootstrap style responsive -->	
+		<link href="themes/css/bootstrap-responsive.min.css" rel="stylesheet"/>
+		<link href="themes/css/font-awesome.css" rel="stylesheet" type="text/css">
+		<!-- Google-code-prettify -->	
+		<link href="themes/js/google-code-prettify/prettify.css" rel="stylesheet"/>
+		<!-- fav and touch icons -->
+		<link rel="shortcut icon" href="themes/images/ico/favicon.ico">
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="themes/images/ico/apple-touch-icon-144-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="themes/images/ico/apple-touch-icon-114-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="themes/images/ico/apple-touch-icon-72-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" href="themes/images/ico/apple-touch-icon-57-precomposed.png">
+		<style type="text/css" id="enject"></style>
+	</head>
 <body>
 <div id="header">
-<div class="container">
-<div id="welcomeLine" class="row">
-	<div class="span6">Welcome!<strong> User</strong></div>
-	<div class="span6">
-	<div class="pull-right">
-	<?php
-		if ( empty($_SESSION['namalengkap']) AND empty($_SESSION['passuser']) ){
-			?>
-				<a href="media2.php?module=daftarmember"><span class="btn btn-mini btn-primary">Register</span></a>
-			<?php
-		}
-		  
-	?>
-		<a href="media2.php?module=keranjangbelanja"><span class="btn btn-mini btn-primary"><i class="icon-shopping-cart icon-white"></i> Keranjang Belanja </span> </a> 
-	</div>
-	</div>
-</div>
+	<div class="container">
+		<div id="welcomeLine" class="row">
+			<div class="span6">Welcome!<strong> User</strong></div>
+			<div class="span6">
+				<div class="pull-right">
+					<?php
+						if ( empty($_SESSION['namalengkap']) AND empty($_SESSION['passuser']) ){
+							?>
+								<a href="media2.php?module=daftarmember"><span class="btn btn-mini btn-primary">Register</span></a>
+							<?php
+						}
+						
+					?>
+					<a href="media2.php?module=keranjangbelanja"><span class="btn btn-mini btn-primary"><i class="icon-shopping-cart icon-white"></i> Keranjang Belanja </span> </a> 
+				</div>
+			</div>
+		</div>
+		
 <!-- Navbar ================================================== -->
 <div id="logoArea" class="navbar">
 <a id="smallScreen" data-target="#topMenu" data-toggle="collapse" class="btn btn-navbar">
