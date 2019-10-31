240a5b933484d06fc2ea8d86d700d819d602627c
csynoers <3s0c9m7@gmail.com>
2019-11-01 02:23:18 +0700

    update media.php

--- media.php
+++ media.php
@@ -2,16 +2,6 @@
     //start session
     session_start();
 
-    function read_file()
-    {
-        return json_decode( file_get_contents("json/logo.json") );
-    }
-    function overwrite_file($rows)
-    {
-        return file_put_contents("json/logo.json", json_encode($rows) );
-    }
-    overwrite_file([1,2,3,4,5]);
-
     /* load config required */
     include_once "config/koneksi.php";
 	include_once "config/fungsi_thumb.php";
@@ -20,13 +10,14 @@
 	include_once "config/fungsi_combobox.php";
 	include_once "config/library.php";
 	include_once "config/fungsi_rupiah.php";
+	include_once "config/helper_file.php";
 	$succesUrl = $serverUrlAndPath."success.php";
 	$failUrl = $serverUrlAndPath."fail.php";
     $statusUrl = $serverUrlAndPath."status.php";
 
-    echo '<pre>';
-	print_r(read_file());
-	echo '</pre>';
+    // echo '<pre>';
+	// print_r(read_file());
+	// echo '</pre>';
     
     /* load template */
     include_once('template.php');
