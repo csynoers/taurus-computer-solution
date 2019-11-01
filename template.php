499ba6582a721f2b58adf30fe093041d2006a56c
csynoers <3s0c9m7@gmail.com>
2019-11-01 11:27:15 +0700

    dinamic slider

--- template.php
+++ template.php
@@ -117,22 +117,19 @@
 	<div id="myCarousel" class="carousel slide">
 		<div class="container">
 			<div class="carousel-inner">
-		  
-				<div class="item">
-					<img style="width:100%" src="themes/images/carousel/1.png" alt=""/>
-					<div class="carousel-caption">
-						<h4>Second Thumbnail label</h4>
-						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-					</div>
-				</div>
-				<div class="item">
-					<img src="themes/images/carousel/2.png" alt=""/>
-					<div class="carousel-caption">
-						<h4>Second Thumbnail label</h4>
-						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-					</div>
-			
-				</div>
+				<?php
+					foreach ( read_file('json/slideshow.json') as $key => $value) {
+						echo '
+							<div class="item">
+								<img style="width:100%" src="src/slideshow/'.$value.'" alt=""/>
+								<div class="carousel-caption">
+									<h4>title</h4>
+									<p>ddescription</p>
+								</div>
+							</div>
+						';
+					}  
+				?>
 			</div>
 		</div>
 
