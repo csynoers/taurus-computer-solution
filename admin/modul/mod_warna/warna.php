83f1779f3e73348a88d7225893a75dac8cec14e1
csynoers <3s0c9m7@gmail.com>
2019-11-14 19:39:46 +0700

    halaman admin menu support->warna produk

--- admin/modul/mod_warna/warna.php
+++ admin/modul/mod_warna/warna.php
@@ -1,41 +1,52 @@
 <?php
     $aksi="modul/mod_merk/aksi_merk.php";
     switch($_GET['act']){
-        
-    // Tampil merk
-    default:
-    echo "<div class='col-xs-12'>
-        <div class='box'>
-            <div class='box-header'>
-              <h3 class='box-title'>MERK</h3>
-            </div>
-			<button type=button class='btn btn-primary' onclick=\"window.location.href='?module=merk&act=tambahmerk';\"><i class='fa fa-plus'> Tambah</i></button>
-							<p>
-            <!-- /.box-header -->
-            <div class='box-body'>
-			<div class='box-body table-responsive no-padding'>
-              <table id='example1' class='table table-bordered table-striped'>  
-  <div class='panel-heading'>
-	<thead>
-          <tr><th>No</th><th>Nama Merk</th><th>Aksi</th></tr>
-		  <tbody>"; 
-    $tampil=mysql_query("SELECT * FROM merk ORDER BY id_merk DESC");
-    $no=1;
-    while ($r=mysql_fetch_array($tampil)){
-       echo "<tr><td>$no</td>
-             <td>$r[nama_merk]</td>
-             <td><a href=?module=merk&act=editmerk&id=$r[id_merk] class='btn btn-warning btn-xs' title='Edit'><i class='fa fa-edit'></i> Edit</a>
-	               <a href=$aksi?module=merk&act=hapus&id=$r[id_merk] class='btn btn-danger btn-xs' title='Hapus' onClick=\"return confirm('Apakah Anda Yakin Untuk Menghapus Data Ini ?')\"><i class='fa fa-trash'></i> Hapus</a>
-             </td></tr>";
-      $no++;
-    }
-    echo "</tbody></table></div>
-	       </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-        </div>";
-    break;
+        default:
+            $htmls = [];
+            $no = 1;
+            foreach ( read_file('../json/warna.json') as $key => $value) {
+                $htmls['tablerows'] .= "
+                    <tr>
+                        <td>{$no}</td>
+                        <td>{$value}</td>
+                        <td>
+                            <a href=?module=merk&act=edit&id= class='btn btn-warning btn-xs' title='Edit'><i class='fa fa-edit'></i> Edit</a>
+                            <a href=$aksi?module=merk&act=hapus&id= class='btn btn-danger btn-xs' title='Hapus' onClick=\"return confirm('Apakah Anda Yakin Untuk Menghapus Data Ini ?')\"><i class='fa fa-trash'></i> Hapus</a>
+                        </td>
+                    </tr>
+                ";
+                $no++;
+            }
+
+            echo "
+                <div class='col-xs-12'>
+                    <div class='box'>
+                        <div class='box-header'>
+                            <h3 class='box-title'>Warna Produk</h3>
+                        </div>
+                        <!-- /.box-header -->
+
+                        <div class='box-body'>
+                            <div class='box-body table-responsive no-padding'>
+                                <table id='example1' class='table table-bordered table-striped'>
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Warna</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>{$htmls["tablerows"]}</tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                </div>
+            ";
+            
+            break;
   
   // Form Tambah merk
   case "tambahmerk":
