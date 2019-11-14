cf7d028515dde543dac0bf6e6409ceb324e3b2c4
csynoers <3s0c9m7@gmail.com>
2019-11-14 22:35:26 +0700

    Fix keranjang belanja 12

--- kanan.php
+++ kanan.php
@@ -456,7 +456,7 @@ elseif ($_GET['module']=='keranjangbelanja'){
 					</td>
 					<td>
 						{$r['nama_produk']}
-						<div>{$produk_attr}</div>
+						<div style='display: inline-flex;'>{$produk_attr}</div>
 					</td>
 					<td><input type=number name='jml[$no]' value='{$r['jumlah']}' size=1 min='1' onChange='this.form.submit()'></td>
 					<td>Rp. $harga</td>
