f61bf668053b6c947bf6fcc33d577065c8c472ad
csynoers <3s0c9m7@gmail.com>
2019-11-20 23:26:18 +0700

    add event onchange select option provinsi target is select option kota debug 2

--- cek_kabupaten.php
+++ cek_kabupaten.php
@@ -5,7 +5,7 @@ require_once 'vendor/autoload.php';
 $name ='';
 $provinsi_id = $_GET['q'];
 $data = RajaOngkir\RajaOngkir::Kota()->byProvinsi($provinsi_id)->search('city_name', $name)->get();
-print_r(RajaOngkir\RajaOngkir::Kota()->byProvinsi($provinsi_id));
+print_r(RajaOngkir\RajaOngkir::Kota()->byProvinsi($provinsi_id)->get());
 foreach ($data as $key => $value) {
   echo'<option value="'.$value['city_id'].'">'.$value['city_name'].'</option>';
 }
