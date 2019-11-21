b35beff0d5434f357c2a40d77139f4d2d2b381e3
csynoers <3s0c9m7@gmail.com>
2019-11-21 20:43:52 +0700

    debug php mailer 3

--- kanan.php
+++ kanan.php
@@ -375,8 +375,27 @@ elseif ($_GET['module']=='daftaraksi'){
 		");
 
 		/* ========== START SEND EMAIL ========== */
-		require_once 'library/phpmailer/autoload.php';
+		require_once("classes/class.phpmailer.php");
+		$mail = new PHPMailer; 
+		$mail->IsSMTP();
+		$mail->SMTPSecure = 'ssl'; 
+		$mail->Host = "smtp.gmail.com";
+		$mail->SMTPDebug = 2;
+		$mail->Port = 465;
+		$mail->SMTPAuth = true;
+		$mail->Username = "3s0c9m7@gmail.com";
+		$mail->Password = $mail->simple_crypt( 'Q28vcmJtWmxESE1UQjBCazFQL2w3QT09', 'd' );
+		$mail->SetFrom("info@tauruscomputer.com","TAURUS COMPUTER");
+		$mail->Subject = "Verifikasi Email";
+		$mail->AddAddress("scmrumahweb@gmail.com","nama email tujuan");
+		$mail->MsgHTML("Testing...");
+
 		print_r($mail);
+		
+		// $mail->Send()
+		// Silakan verifikasi email kamu dengan mengklik / copy
+		// tautan berikut kedalam browser :
+
 		/* ========== END SEND EMAIL ========== */
 
 		// echo"							
