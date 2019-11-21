58fe51f28d5a159a02ec4506b1292f0f04909e84
csynoers <3s0c9m7@gmail.com>
2019-11-21 10:31:57 +0700

    tes send email aksi daftar 3

--- kanan.php
+++ kanan.php
@@ -376,38 +376,11 @@ elseif ($_GET['module']=='daftaraksi'){
 
 		/* ========== START SEND EMAIL ========== */
 		$to = "{$_POST['email']}";
-		$subject = "HTML email";
+		$subject = "My subject";
+		$txt = "Hello world!";
+		$headers = "From: webmaster@example.com" . "\r\n" ;
 
-		$message = "
-		<html>
-			<head>
-				<title>HTML email</title>
-			</head>
-			<body>
-			<p>This email contains HTML Tags!</p>
-			<table>
-			<tr>
-			<th>Firstname</th>
-			<th>Lastname</th>
-			</tr>
-			<tr>
-			<td>John</td>
-			<td>Doe</td>
-			</tr>
-			</table>
-			</body>
-		</html>
-		";
-
-		// Always set content-type when sending HTML email
-		$headers = "MIME-Version: 1.0" . "\r\n";
-		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-		// More headers
-		// $headers .= 'From: <webmaster@example.com>' . "\r\n";
-		// $headers .= 'Cc: myboss@example.com' . "\r\n";
-
-		mail($to,$subject,$message,$headers);
+		mail($to,$subject,$txt,$headers);
 		/* ========== END SEND EMAIL ========== */
 
 		// echo"							
