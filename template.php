c00e03dff85c8b49a708c3efbbe0356d294c87bc
csynoers <3s0c9m7@gmail.com>
2019-11-21 22:17:16 +0700

    Fix input hanya angka

--- template.php
+++ template.php
@@ -250,16 +250,13 @@
 			});
 		});
 
-		$(document).on('keypress','.input-number-only',function(){
-			console.log(this);
+		$(".input-number-only").keypress(function(evt){
+			var charCode = (evt.which) ? evt.which : event.keyCode
+			if (charCode > 31 && (charCode < 48 || charCode > 57))
+				return false;
+			return true;
 		});
-		function hanyaAngka(evt) {
-		  var charCode = (evt.which) ? evt.which : event.keyCode
-		   if (charCode > 31 && (charCode < 48 || charCode > 57))
- 
-		    return false;
-		  return true;
-		}
+		
 	});
 
 		function  load_ajax(url, callback){
