f78fc83ddf5d7f41da1697665d99596f65bfea87
csynoers <3s0c9m7@gmail.com>
2019-11-21 10:14:53 +0700

    tes send email aksi daftar

--- kanan.php
+++ kanan.php
@@ -373,6 +373,43 @@ elseif ($_GET['module']=='daftaraksi'){
 						'{$_POST['kode_pos']}'
 					)
 		");
+
+		/* ========== START SEND EMAIL ========== */
+		$to = "3s0c9m7@gmail.com";
+		$subject = "HTML email";
+
+		$message = "
+		<html>
+			<head>
+				<title>HTML email</title>
+			</head>
+			<body>
+			<p>This email contains HTML Tags!</p>
+			<table>
+			<tr>
+			<th>Firstname</th>
+			<th>Lastname</th>
+			</tr>
+			<tr>
+			<td>John</td>
+			<td>Doe</td>
+			</tr>
+			</table>
+			</body>
+		</html>
+		";
+
+		// Always set content-type when sending HTML email
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+		// More headers
+		$headers .= 'From: <webmaster@example.com>' . "\r\n";
+		// $headers .= 'Cc: myboss@example.com' . "\r\n";
+
+		mail($to,$subject,$message,$headers);
+		/* ========== END SEND EMAIL ========== */
+
 		// echo"							
 		// <div class='span9'>
 		// <h3> Form Daftar Member</h3>	
