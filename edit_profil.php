105670114410c2085e5ca1a92f37ce5759c79f74
csynoers <3s0c9m7@gmail.com>
2019-11-29 09:59:43 +0700

    Fix proses edit member

--- edit_profil.php
+++ edit_profil.php
@@ -1,25 +1,49 @@
 <?php
-session_start();
+    session_start();
+    include_once "config/koneksi.php";
+    $data = [];
+    $data['post'] = $_POST;
 
-include "config/koneksi.php";
+    if ( empty($data['post']['password']) ) {
+        $data['sql_update_member'] = ("
+            UPDATE member
+                SET
+                    nama            = '{$data['post']['nama']}',
+                    email           = '{$data['post']['email']}',
+                    alamat_member   = '{$data['post']['alamat']}',
+                    no_telp         = '{$data['post']['no_telp']}',  
+                    provinsi        = '{$data['post']['provinsi']}',  
+                    kota            = '{$data['post']['kota']}',  
+                    kode_pos        = '{$data['post']['kode_pos']}'  
+                WHERE
+                    id_member       = '{$data['post']['id']}'
+        ");
+    }
+    // Apabila password diubah
+    else{
+        $data['post']['password'] = md5($data['post']['password']);
+        $data['sql_update_member'] = ("
+            UPDATE member
+                SET
+                    password        = '{$data['post']['password'] }',
+                    nama            = '{$data['post']['nama'] }',
+                    alamat_member   = '{$data['post']['alamat']}',
+                    email           = '{$data['post']['email']}',  
+                    no_telp         = '{$data['post']['no_telp']}',  
+                    provinsi        = '{$data['post']['provinsi']}',  
+                    kota            = '{$data['post']['kota']}',  
+                    kode_pos        = '{$data['post']['kode_pos']}'  
+                WHERE
+                    id_member      = '$_POST[id]'");
+    }
 
-if (empty($_POST[password])) {
-    mysql_query("UPDATE member SET nama   = '$_POST[nama]',
-                                  email          = '$_POST[email]',
-								  alamat_member    = '$_POST[alamat]',
-                                  no_telp        = '$_POST[no_telp]'  
-                           WHERE  id_member     = '$_POST[id]'");
-  }
-  // Apabila password diubah
-  else{
-    $pass=md5($_POST[password]);
-    mysql_query("UPDATE member SET password        = '$pass',
-                                 nama    = '$_POST[nama]',
-								 alamat_member    = '$_POST[alamat]',
-                                 email           = '$_POST[email]',  
-                                 no_telp         = '$_POST[no_telp]'  
-                           WHERE id_member      = '$_POST[id]'");
-  }
-  echo "<script>window.alert('Data berhasil diubah');
-        window.location=('edit-member.html')</script>";
+    if ( mysql_query($data['sql_update_member']) ) { # TRUE
+        echo "<script>window.alert('Data berhasil diubah');window.location=('edit-member.html')</script>";
+
+    } else { # FALSE
+        echo "<script>window.alert('Data gagal diubah');window.location=('edit-member.html')</script>";
+
+    }
+    
+
 ?>
