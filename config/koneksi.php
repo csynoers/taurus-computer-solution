803be418972232ccca8b969ca375c56d2d682c62
csynoers <3s0c9m7@gmail.com>
2020-01-28 05:08:21 +0700

    update config koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,9 +2,9 @@
     date_default_timezone_set("Asia/Jakarta");
     // error_reporting(0);
     $server   = "localhost";
-    $username = "smpnseda_5c3";
+    $username = "taurusco_5c3";
     $password = "3.s.0.c.9.m.7";
-    $database = "smpnseda_tauruskomputer";
+    $database = "taurusco_mputer";
 
     // Koneksi dan memilih database di server
     mysql_connect($server,$username,$password) or die("Koneksi gagal");
